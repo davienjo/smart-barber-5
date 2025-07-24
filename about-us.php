2a533506bab8437584af867526ec450a23c162d0
davienjo <66726094+davienjo@users.noreply.github.com>
2025-07-24 23:42:18 +0300

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,96 @@
+<?php
+include('partials/header.php');
+?>
+
+
+  <section class="about-section">
+    <div class="about-images">
+      <img class=" grid-img large-img" src="images/jason-leung-2seUdPQNy_I-unsplash.jpg" alt="" />
+      <img class="grid-img" src="images/michael-demoya-MUdB4YzDeKA-unsplash.jpg" alt="" />
+      <img class="grid-img" src="images/michael-demoya-Q82AM6BWBPM-unsplash.jpg" alt="" />
+
+
+    </div>
+
+    <div class="about-info">
+      <div class="about-box">
+        <h2>Our Barbers</h2>
+        <p>
+          We are a team of passionate and skilled barbers with years of<br>
+          experience in classic and modern styles. Each member of our team<br>
+          is trained to give you the best haircut, fade, or beard trim —<br>
+          tailored to your unique style.
+        </p>
+      </div>
+
+      <div class="about-box">
+        <h2>Our History</h2>
+        <p>
+          Founded in 2015, Smart-Barber began as a one-chair shop in<br>
+          California, USA. Today, we've grown into a full-service grooming<br>
+          destination known for precision, customer care, and good vibes.<br>
+          Our mission has always been to make you feel and look your best.<br>
+        </p>
+      </div>
+
+      <div class="about-box">
+        <h2>Our Location</h2>
+        <p>
+          We are Located at San Diego near Hill Crest - Open Monday to<br>
+          Saturday from 8:00 AM to 8:00 PM.
+        </p>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Team members -->
+
+  <h2 class="members-h2">Meet our team of experts</h2>
+  <section class="team-members">
+
+    <div class="member">
+      <div class="member-img">
+        <img src="images/member 2.png" alt="member 1">
+        <div class="details">
+          <p class="name">Martin Lawrence</p>
+          <p class="role">Senior Barber</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="member">
+      <div class="member-img">
+        <img src="images/member 1.png" alt="member 2">
+        <div class="details">
+          <p class="name">John Smith</p>
+          <p class="role">Hair Stylist</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="member">
+      <div class="member-img">
+        <img src="images/member 3.png" alt="member 3">
+        <div class="details">
+          <p class="name">James Brown</p>
+          <p class="role">Beard Specialist</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="member">
+      <div class="member-img">
+        <img src="images/member 4.png" alt="member 4">
+        <div class="details">
+          <p class="name">Kelvin Squidward</p>
+          <p class="role">Fade Artist</p>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+  <?php
+  include('partials/footer.php');
+  ?>
